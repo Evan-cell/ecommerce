0906952e0c589e0f5bc6f068162646bc071841b4
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-02 11:09:12 +0300

    shipping address

--- store/models.py
+++ store/models.py
@@ -31,3 +31,17 @@ class OrderItem(models.Model):
        order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
        quantity = models.IntegerField(default=True, null=True, blank=True)
        date_added = models.DateTimeField(auto_now_add=True)
+       
+       def __str__(self):
+            return self.product
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)  
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)     
+    address =models.CharField(max_length=200, null=True)
+    city =models.CharField(max_length=200, null=True)
+    county =models.CharField(max_length=200, null=True)
+    zipcode =models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
