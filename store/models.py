073f42871f469bd2778b3a6deb19156c95c2eb16
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-22 10:13:35 +0300

    blogpost

--- store/models.py
+++ store/models.py
@@ -50,6 +50,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(max_length=3000)
     image = CloudinaryField('image')
+    User = models.CharField(max_length=150)
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
