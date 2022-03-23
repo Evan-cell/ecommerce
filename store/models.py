831c7827ff99e98b0f5319df727e9305fd0f8dd5
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-23 07:35:31 +0300

    query items

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class OrderItem(models.Model):
        date_added = models.DateTimeField(auto_now_add=True)
        
        def __str__(self):
-            return self.product
+            return str(self.product)
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)  
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)     
