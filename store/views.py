02e6dcc5b4006192214b241de34f81456a7c8a2a
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-24 10:12:02 +0300

    tota items

--- store/views.py
+++ store/views.py
@@ -6,9 +6,21 @@ import json
 # Create your views here.
 
 def store(request):
-    products = Product.objects.all()
-    context = {"products":products}
-    return render(request,'store/store.html',context)
+    
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.filter(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
 def cart(request):
     
 	if request.user.is_authenticated:
