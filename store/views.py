f591f30c485aaf4f7d3d9c3a4257c73dc7b043ce
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-24 12:08:28 +0300

    proccess data

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -94,3 +95,33 @@ def contact(request):
 def about(request):
     context = {}
     return render(request, 'home/about.html', context)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment submitted..', safe=False)    
+    	
+               
+    
